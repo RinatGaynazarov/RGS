3e660d772330c4734a54b72a13f7f4411200bcd2
rinat <ttps://github.com/RinatGaynazarov/RGS.git>
2025-10-06 15:04:18 +0600

    updete urls_

--- RGS/settings.py
+++ RGS/settings.py
@@ -130,6 +130,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
